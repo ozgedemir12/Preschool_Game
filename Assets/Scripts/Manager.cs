0a84eb076f4c2619f809be02c6d5c67b0122fb5a
Özge <56171697+ozgedemir12@users.noreply.github.com>
2021-05-31 16:16:39 +0300

    Preschol

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject circle, diamond, hexagon, rectangle, triangle, circleblack, diamondblack, hexagonblack, rectangleblack, triangleblack;
+
+    
+    public Button btnn;
+    public Button btn;
+    
+
+    Vector2 circleInitialPos, diamondInitialPos, hexagonInitialPos, rectangleInitialPos, triangleInitialPos;
+
+    bool circleCorrect, diamondCorrect, hexagonCorrect, rectangleCorrect, triangleCorrect = false;
+
+    void Start()
+    {
+        circleInitialPos = circle.transform.position;
+        diamondInitialPos = diamond.transform.position;
+        hexagonInitialPos = hexagon.transform.position;
+        rectangleInitialPos = rectangle.transform.position;
+        triangleInitialPos = triangle.transform.position;
+    }
+    public void DragCircle()
+    {
+        circle.transform.position = Input.mousePosition;
+    }
+    public void DragDiamond()
+    {
+        diamond.transform.position = Input.mousePosition;
+    }
+    public void DragHexagon()
+    {
+        hexagon.transform.position = Input.mousePosition;
+    }
+    public void DragRectangle()
+    {
+        rectangle.transform.position = Input.mousePosition;
+    }
+    public void DragTriangle()
+    {
+        triangle.transform.position = Input.mousePosition;
+    }
+
+
+    public void DropCircle()
+    {
+        float Distance = Vector3.Distance(circle.transform.position, circleblack.transform.position);
+        if (Distance < 50)
+        {
+            circle.transform.position = circleblack.transform.position;
+            circleCorrect = true;
+        }
+        else
+        {
+            circle.transform.position = circleInitialPos;
+        }
+    }
+    public void DropDiamond()
+    {
+        float Distance = Vector3.Distance(diamond.transform.position, diamondblack.transform.position);
+        if (Distance < 50)
+        {
+            diamond.transform.position = diamondblack.transform.position;
+            diamondCorrect = true;
+        }
+        else
+        {
+            diamond.transform.position = diamondInitialPos;
+        }
+
+    }
+    public void DropHexagon()
+    {
+        float Distance = Vector3.Distance(hexagon.transform.position, hexagonblack.transform.position);
+        if (Distance < 50)
+        {
+            hexagon.transform.position = hexagonblack.transform.position;
+            hexagonCorrect = true;
+        }
+        else
+        {
+            hexagon.transform.position = hexagonInitialPos;
+        }
+    }
+    public void DropRectangle()
+    {
+        float Distance = Vector3.Distance(rectangle.transform.position, rectangleblack.transform.position);
+        if (Distance < 50)
+        {
+            rectangle.transform.position = rectangleblack.transform.position;
+            rectangleCorrect = true;
+        }
+        else
+        {
+            rectangle.transform.position = rectangleInitialPos;
+        }
+    }
+    public void DropTriangle()
+    {
+        float Distance = Vector3.Distance(triangle.transform.position, triangleblack.transform.position);
+        if (Distance < 50)
+        {
+            triangle.transform.position = triangleblack.transform.position;
+            triangleCorrect = true;
+        }
+        else
+        {
+            triangle.transform.position = triangleInitialPos;
+        }
+    }
+     void Update()
+    {
+        if (circleCorrect & diamondCorrect & hexagonCorrect & rectangleCorrect & triangleCorrect)
+        {
+            btnn.gameObject.SetActive(true);
+            btn.gameObject.SetActive(true);
+            
+            
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Scene2.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scene2 : MonoBehaviour
+{
+    public GameObject beans, carrot, cucumbers, eggplant, lettuce, beansblack, carrotblack, cucumbersblack, eggplantblack, lettuceblack;
+
+
+    Vector2 beansInitialPos, carrotInitialPos, cucumbersInitialPos, eggplantInitialPos, lettuceInitialPos;
+
+    bool beansCorrect, carrotCorrect, cucumbersCorrect, eggplantCorrect, lettuceCorrect = false;
+
+    public Button btn;
+    public Button btnn;
+
+    void Start()
+    {
+        beansInitialPos = beans.transform.position;
+        carrotInitialPos = carrot.transform.position;
+        cucumbersInitialPos = cucumbers.transform.position;
+        eggplantInitialPos = eggplant.transform.position;
+        lettuceInitialPos = lettuce.transform.position;
+    }
+    public void DragBeans()
+    {
+        beans.transform.position = Input.mousePosition;
+    }
+    public void DragCarrot()
+    {
+        carrot.transform.position = Input.mousePosition;
+    }
+    public void DragCucumbers()
+    {
+        cucumbers.transform.position = Input.mousePosition;
+    }
+    public void DragEggplant()
+    {
+        eggplant.transform.position = Input.mousePosition;
+    }
+    public void DragLettuce()
+    {
+        lettuce.transform.position = Input.mousePosition;
+    }
+
+
+    public void DropBeans()
+    {
+        float Distance = Vector3.Distance(beans.transform.position, beansblack.transform.position);
+        if (Distance < 50)
+        {
+            beans.transform.position = beansblack.transform.position;
+            beansCorrect = true;
+        }
+        else
+        {
+            beans.transform.position = beansInitialPos;
+        }
+    }
+    public void DropCarrot()
+    {
+        float Distance = Vector3.Distance(carrot.transform.position, carrotblack.transform.position);
+        if (Distance < 50)
+        {
+            carrot.transform.position = carrotblack.transform.position;
+            carrotCorrect = true;
+        }
+        else
+        {
+            carrot.transform.position = carrotInitialPos;
+        }
+
+    }
+    public void DropCucumbers()
+    {
+        float Distance = Vector3.Distance(cucumbers.transform.position, cucumbersblack.transform.position);
+        if (Distance < 50)
+        {
+            cucumbers.transform.position = cucumbersblack.transform.position;
+            cucumbersCorrect = true;
+        }
+        else
+        {
+            cucumbers.transform.position = cucumbersInitialPos;
+        }
+    }
+    public void DropEggplant()
+    {
+        float Distance = Vector3.Distance(eggplant.transform.position, eggplantblack.transform.position);
+        if (Distance < 50)
+        {
+            eggplant.transform.position = eggplantblack.transform.position;
+            eggplantCorrect = true;
+        }
+        else
+        {
+            eggplant.transform.position = eggplantInitialPos;
+        }
+    }
+    public void DropLettuce()
+    {
+        float Distance = Vector3.Distance(lettuce.transform.position, lettuceblack.transform.position);
+        if (Distance < 50)
+        {
+            lettuce.transform.position = lettuceblack.transform.position;
+            lettuceCorrect = true;
+        }
+        else
+        {
+            lettuce.transform.position = lettuceInitialPos;
+        }
+    }
+    void Update()
+    {
+        if (beansCorrect & carrotCorrect & cucumbersCorrect & eggplantCorrect & lettuceCorrect)
+        {
+            btn.gameObject.SetActive(true);
+            btnn.gameObject.SetActive(true);
+         
+
+        }
+    }
+
+}
